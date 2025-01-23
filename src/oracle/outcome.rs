9eaab6a95a96f49b8986bb5d9c2179141ab173ea
raphjaph <raphjaph@protonmail.com>
2025-01-23 12:50:39 +0100

    Add normalization test

--- src/oracle/outcome.rs
+++ src/oracle/outcome.rs
@@ -25,13 +25,79 @@ impl Outcome {
     // https://github.com/discreetlogcontracts/dlcspecs/blob/master/Oracle.md#serialization-and-signing-of-outcome-values
     let normalized_label = self.label.nfc().collect::<String>();
     let tagged_hash = tagged_hash(ATTESTATION_TAG, normalized_label.as_bytes());
-    let message = Message::from_digest_slice(&tagged_hash)?;
 
     Ok(schnorrsig_sign_with_nonce(
       &secp,
-      &message,
+      &Message::from_digest(tagged_hash),
       &keypair,
       &self.secret_nonce,
     ))
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use {super::*, serde_json::json};
+
+  #[test]
+  fn labels_are_normalized_correctly() {
+    // From https://github.com/dgarage/NDLC/blob/d816c0c517611b336f09ceaa43d400ecb5ab909b/NDLC.Tests/Data/normalization_tests.json
+    let data = json!([
+      {
+        "Description": "Singleton",
+        "Variants": [ "\u{212b}", "\u{0041}\u{030a}", "\u{00c5}" ],
+        "Expected": "c385",
+        "SHA256": "0a94dc9d420d1142d6b71de60f9bf7e2f345a4d62c9f141b091539769ddf3075"
+      },
+      {
+        "Description": "Canonical Composites",
+        "Variants": [ "\u{00f4}", "\u{006f}\u{0302}", "\u{00f4}" ],
+        "Expected": "c3b4",
+        "SHA256": "cc912dbca598fd80ca7f5d98ece5d846b447f4a9ae3f73c352e2687eb293eef5"
+      },
+      {
+        "Description": "Multiple Combining Marks",
+        "Variants": [ "\u{1e69}", "\u{0073}\u{0323}\u{0307}", "\u{1e69}" ],
+        "Expected": "e1b9a9",
+        "SHA256": "ceca1ea456e95ee498463622915209bb08a018e8ee9741b46b64ef1a08fb56ab"
+      },
+      {
+        "Description": "Compatibility Composites",
+        "Variants": [ "\u{fb01}" ],
+        "Expected": "efac81",
+        "SHA256": "b6554cce8a93f1c8818280e2a768116a79216ad5501a85357d233409db87d340"
+      },
+      {
+        "Description": "Non Composites",
+        "Variants": [ "fi" ],
+        "Expected": "6669",
+        "SHA256": "b4bdc848109722a383d0a972c6eb859f2abd29565b8c4cc7199e7c9eb708f1b7"
+      },
+      {
+        "Description": "Random string",
+        "Variants": [ "éléphant" ],
+        "Expected": "c3a96cc3a97068616e74",
+        "SHA256": "c941ae685f62cbe7bb47d0791af7154788fd9e873e5c57fd2449d1454ed5b16f"
+      }
+    ]);
+
+    for test in data.as_array().unwrap() {
+      for variant in test["Variants"].as_array().unwrap() {
+        let normalized = variant.as_str().unwrap().nfc().collect::<String>();
+
+        let hex = normalized
+          .bytes()
+          .map(|b| format!("{:02x}", b))
+          .collect::<String>();
+
+        assert_eq!(test["Expected"].as_str().unwrap(), hex);
+
+        let expected_bytes = hex::decode(test["Expected"].as_str().unwrap()).unwrap();
+
+        let sha256_hash = sha256::Hash::hash(&expected_bytes).to_string();
+
+        assert_eq!(test["SHA256"].as_str().unwrap(), sha256_hash);
+      }
+    }
   }
 }
--- src/subcommand/verify.rs
+++ src/subcommand/verify.rs
@@ -15,10 +15,13 @@ pub(crate) struct Verify {
 impl Verify {
   pub(crate) fn run(self) -> Result {
     let tagged_hash = tagged_hash(ORACLE_TAG, self.message.as_bytes());
-    let message = Message::from_digest(tagged_hash);
     let public_key = XOnlyPublicKey::from_str(&self.public_key)?;
     let signature = Signature::from_str(&self.signature)?;
 
-    Ok(Secp256k1::verification_only().verify_schnorr(&signature, &message, &public_key)?)
+    Ok(Secp256k1::verification_only().verify_schnorr(
+      &signature,
+      &Message::from_digest(tagged_hash),
+      &public_key,
+    )?)
   }
 }
